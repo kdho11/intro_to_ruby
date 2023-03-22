2a78565ee082456c88959d66fcc4425e8e6d37e6
kendall holland <holland.kd@gmail.com>
2023-03-22 15:34:22 -0400

    methods chapter

---
+++ 03_methods/exercises.rb
@@ -0,0 +1,18 @@
+# def greeting(name) 
+#  "Hi #{name}!"
+# end
+
+# puts greeting("Kendall")
+
+# def multiply(a,b)
+#   a * b 
+# end
+
+# puts multiply(2,3)
+
+def scream(words)
+  words = words + "!!!!"
+  p words
+end
+
+scream("Yippeee")
---
+++ 03_methods/return.rb
@@ -0,0 +1,14 @@
+def add_three(number)
+  return number + 3
+   number + 4
+end
+
+returned_value = add_three(4)
+puts returned_value
+
+def add_three(n)
+  puts n + 3
+  return n + 3
+end
+
+add_three(5).times {puts 'this will print 8 times'}
