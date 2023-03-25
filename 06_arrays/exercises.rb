8c2099b0695d1b417abfa88cf311b1da251faa4a
kendall holland <holland.kd@gmail.com>
2023-03-25 11:17:01 -0400

    starting arrays chapter

---
+++ 06_arrays/exercises.rb
@@ -0,0 +1,10 @@
+# arr = [1,3,5,7,9,11]
+# number = 3
+# puts "Yes" if arr.include?(number)
+ arr = ["b", "a"]
+ arr = arr.product(Array(1..3))
+ arr.first.delete(arr.first.last)
+
+# 2. arr = ["b", "a"]
+#    arr = arr.product([Array(1..3)])
+#    arr.first.delete(arr.first.last)
